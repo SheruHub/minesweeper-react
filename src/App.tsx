f90282b03b5fdd62530a874d8346ec8bac42f030
SheruHub <n4383079@qut.edu.au>
2021-07-11 17:05:09 +1000

    Adding some settings options

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,45 @@ import './App.css';
 import { DrawNodes } from './components/DrawNodes';
 import * as Game from './logic/GameLogic'
 
-
 function App() {
+   const defaultGame = {
+      width: '10',
+      height: '10',
+      bombs: '20'
+   }
    const game = Game.InitBoard(10, 10, 20)
    const [gameState, changeGameState] = useState(game)
 
+   const resetGame = () => {
+      changeGameState(Game.InitBoard(10, 10, 20))
+   }
+
    return (
       <div className="App">
-         <DrawNodes gameBoard={gameState} changeGameBoard={changeGameState} />
-      </div>
+         <h1>Minesweeper</h1>
+         <form>
+            <label>Grid width:
+               <input type="text" name="width" id="width" value={defaultGame.width} />
+            </label>
+            <label>Grid height:
+               <input type="text" name="height" id="height" value={defaultGame.height} />
+            </label>
+            <label># Bombs:
+               <input type="text" name="numBombs" id="numBombs" value={defaultGame.bombs} />
+            </label>
+            <input type="submit" value="Generate" />
+         </form>
+         <hr />
+         <button onClick={() => resetGame()} >Reset Game</button>
+         <hr />
+         <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+         }}>
+            <DrawNodes gameBoard={gameState} changeGameBoard={changeGameState} />
+         </div>
+      </div >
    );
 }
 
